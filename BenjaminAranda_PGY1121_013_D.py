957998df127e549c7b5f1fd024337e6b53ed29c0
Benjaminarandaq <134451721+Benjaminarandaq@users.noreply.github.com>
2023-06-19 18:34:06 -0400

    evaluacion BenjaminAranda_PGY1121_013_D
    evaluacon del dia lunes

---
+++ BenjaminAranda_PGY1121_013_D.py
@@ -0,0 +1,51 @@
+def NIF_persona(lista):
+lista = []
+print("introdusca su NIF"):
+
+nif_p1 = int(input("xxxxxxxx")):
+nif_p2 = input("-XXX"):
+
+nombre1 = print("singrese su nombre"):
+nacimiento = print("grese su fecha de nacimiento"):
+
+lista = [nif_p1, nif_p1[2, nombre1, nacimiento]]:
+
+def lista = [2, 2]:
+lista_contenido = lista.insert(index, nif_p1, nif_p2):
+lista_contenido2 = lista.insert(2, nombre1, nacimiento):
+
+if nif_p1 is int(>99999999):
+    print("no es acepttable su NIF"):
+        return
+if nif_p2 is int(>999):
+    print("no es actable su NIF"):
+        return
+if nacimiento is int(2008, 2009, 2010, 2011, 2012, 20013, 2014, 20015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023):
+    print("usted no tiene la edad suficiente")
+        return
+lista = [lista_contenido, lista_contenido2]:
+
+print(f"su nif es {lista}"):
+print("qyuere imprimer su lista?"):
+    input():
+    if input() = si:
+        
+        def imprimir_nacimiento()
+
+        print("introdusxa su estado conyugal")
+            estado_conyugal = input()
+        print("es usted de de la union europea?")
+            pertenencia_union_europea = input()
+         
+        in lista.extend(:: estado conyugal:: pertenencia_union_europea)
+
+        print(f"{lista}")
+            return if input() = no:
+            end
+print("quier hacer o agregar algo mas?")
+    input()
+        if input() = si:
+return
+    if input() = no:
+print("grcias por usar el programa, este mini programa fue echo por benjamin aranda")
+print("ver_1.0")
